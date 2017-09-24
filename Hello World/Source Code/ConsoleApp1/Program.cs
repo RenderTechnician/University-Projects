81b77d0610da0e81474e35918725cefe49930037
Will Rathlou <willrathlou@hotmail.com>
2017-09-24 13:37:01 +0100

    Hello World

---
+++ Hello World/Source Code/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine(DateTime.Now.ToString());
+            Console.WriteLine("You can close this application now....");
+            Console.ReadLine();
+        
+            
+        }
+    }
+}
