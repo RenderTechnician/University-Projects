d2ac80ad808dcc425255affdf5ede667d7bcf494
Will Rathlou <willrathlou@hotmail.com>
2017-10-12 11:21:25 +0100

    Array's & Loops [Week 3]
    Printing data from an array using a loop structure.
    
    Odd numbers are printed on line 1
    Even numbers are printed on line 2

---
+++ Array's & Loops [Week 3]/demo week 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace demo_week_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Setter = 0;
+            int[] numbers = {15, 68, 4, 19, 99, 52, 53 ,36, 74, 1, 85 };
+            do
+            {
+                if (numbers[Setter] % 2 != 0)
+                {
+                    Console.Write(numbers[Setter] + " ");
+                }
+                Setter++;
+            }
+
+            while (11 > Setter);
+            Setter = 0;
+            Console.WriteLine();
+            do
+            {
+                if (numbers[Setter] % 2 == 0)
+                {
+                    Console.Write(numbers[Setter] + " ");
+                }
+
+                Setter++;
+            }
+
+            while (11 > Setter);
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
