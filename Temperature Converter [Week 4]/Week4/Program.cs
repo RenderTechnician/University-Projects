133709d9a4f0d1c8f9872ed0882f1f7338e49f0f
Will Rathlou <willrathlou@hotmail.com>
2017-10-24 13:13:46 +0100

    Temperature Converter

---
+++ Temperature Converter [Week 4]/Week4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Week4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahrTemp,celTemp;
+            Console.WriteLine
+            ("Enter a temperature in degrees Fahrenheit");
+            fahrTemp = double.Parse(Console.ReadLine());
+            celTemp = calcTempC(fahrTemp);
+
+            displayTempC(celTemp);
+        }
+     static double calcTempC(double faren)
+        {
+            double C = (faren - 32) * 5 / 9;
+            return C;
+        }
+        static double displayTempC(double Celler)
+        {
+            Console.WriteLine(Celler);
+            Console.ReadLine();
+            return Celler;
+            
+        }
+    }
+}
